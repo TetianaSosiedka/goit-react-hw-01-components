ca961f2a20827c99a0e3ec123b9dbce06fbf7426
TetianaSosiedka <tasha.sosedka@gmail.com>
2022-07-03 12:35:49 +0300

    added TransactionHistory component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import Profile from './profile/Profile';
 import Statistics from './statistics/Statistics';
 import FriendList from './friendList/FriendList';
+import TransactionHistory from './transactionHistory/TransactionHistory';
 
 import user from 'path/to/user.json';
 import data from 'path/to/data.json';
 import friends from 'path/to/friends.json';
+import transactions from 'path/to/transactions.json';
 
 export const App = () => {
   return (
@@ -20,6 +22,7 @@ export const App = () => {
       <Profile user={user} />
       <Statistics title="Upload stats" data={data} />
       <FriendList friends={friends} />
+      <TransactionHistory transactions={transactions} />
     </div>
   );
 };
---
+++ src/components/transactionHistory/TransactionHistory/index.jsx
@@ -0,0 +1,36 @@
+import PropTypes from 'prop-types';
+
+import TransactionHistoryItem from '../TransactionHistoryItem';
+
+const TransactionHistory = ({ transactions }) => {
+  return (
+    <table className="transaction-history">
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {transactions.map(item => {
+          return (
+            <TransactionHistoryItem
+              key={item.id}
+              type={item.type}
+              amount={item.amount}
+              currency={item.currency}
+            />
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+TransactionHistory.prototype = {
+  transactions: PropTypes.arrayOf(),
+};
+
+export default TransactionHistory;
